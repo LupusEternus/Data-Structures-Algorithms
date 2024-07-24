6bc520965263de9226f919fef410ede162a8f3f9
LupusEternus <wilk.tomek@gmail.com>
2024-07-24 13:32:09 +0200

    added solution for structy task : connected components count

---
+++ src/structy/ConnectedComponentsCount.java
@@ -0,0 +1,37 @@
+package structy;
+
+import java.util.*;
+
+/*
+problem:
+Write a method, connectedComponentsCount, that takes in the adjacency list of an undirected graph.
+The method should return the number of connected components within the graph.
+solution:
+using DepthSearchFirst algorithm;
+*/
+
+
+class ConnectedComponentsCount {
+    public static int connectedComponentsCount(Map<Integer, List<Integer>> graph) {
+        Set<Integer> visited = new HashSet<>();
+        int counter = 0;
+        for (Map.Entry<Integer, List<Integer>> entry : graph.entrySet()) {
+            Stack<Integer> stack = new Stack<>();
+            if (!visited.contains(entry.getKey())) {
+                stack.push(entry.getKey());
+                while (!stack.empty()) {
+                    Integer current = stack.pop();
+                    visited.add(current);
+                    for (Integer i : graph.get(current)) {
+                        if (!visited.contains(i)) {
+                            stack.push(i);
+                        }
+                    }
+                }
+                counter++;
+            }
+        }
+        return counter;
+    }
+}
+
