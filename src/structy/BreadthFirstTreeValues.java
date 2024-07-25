643a00ce9149d3f3f6fbf48688ec2f0d5a6db5c2
LupusEternus <wilk.tomek@gmail.com>
2024-07-25 13:17:48 +0200

    added solution for structy task- BreadthFirstTreeValues and DepthFirstTreeValues

---
+++ src/structy/BreadthFirstTreeValues.java
@@ -0,0 +1,25 @@
+package structy;
+
+import utils.Node;
+import java.util.*;
+
+public class BreadthFirstTreeValues {
+
+    public static List<String> breadthFirstValues(Node<String> root) {
+        List<String> values = new ArrayList<>();
+        Queue<Node<String>> que = new LinkedList<>();
+        if (root == null) return values;
+        que.add(root);
+        while (!que.isEmpty()) {
+            Node<String> current = que.poll();
+            values.add(current.val);
+            if (current.left != null) {
+                que.add(current.left);
+            }
+            if (current.right != null) {
+                que.add(current.right);
+            }
+        }
+        return values;
+    }
+}
---
+++ src/structy/DepthFirstTreeValues.java
@@ -0,0 +1,27 @@
+package structy;
+
+import utils.Node;
+
+import java.util.*;
+
+public class DepthFirstTreeValues {
+    public static List<String> depthFirstValues(Node<String> root) {
+        Stack<Node<String>> stack = new Stack<>();
+        List<String> values = new ArrayList<>();
+        if (root == null) return values;
+        stack.push(root);
+        while (!stack.empty()) {
+            Node<String> currentNode = stack.pop();
+            values.add(currentNode.val);
+            if (currentNode.right != null) {
+                stack.push(currentNode.right);
+            }
+            if (currentNode.left != null) {
+                stack.push(currentNode.left);
+            }
+        }
+        return values;
+    }
+}
+
+
